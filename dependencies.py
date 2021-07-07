9b69fa951af08ffac90b8eed0ba789c2a0ff884b
koteshrv <kotesh.rv@gmail.com>
2021-07-08 01:46:13 +0530

    updated chrome options

--- dependencies.py
+++ dependencies.py
@@ -50,7 +50,8 @@ options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument("--disable-infobars")
 options.add_argument("--window-size=1200,800")
-options.add_argument("user-agent='User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'")
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
 options.add_experimental_option("prefs", { \
     "profile.default_content_setting_values.media_stream_mic": 2,     # 1:allow, 2:block
     "profile.default_content_setting_values.media_stream_camera": 2,

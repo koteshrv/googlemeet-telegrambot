972690cb3a176e31508ee03d219f85b5700bc5a3
koteshrv <kotesh.rv@gmail.com>
2021-07-08 00:42:07 +0530

    updated environ error

--- login.py
+++ login.py
@@ -3,6 +3,6 @@ import os
 
 # used to login to gmail 
 # we use this only once while setting chrome profile
-mail = os.environ.get['MAIL_ID']
-password = os.environ.get['PASSWORD']
+mail = os.environ.get('MAIL_ID')
+password = os.environ.get('PASSWORD')
 login(mail, password)

03edb112506e04c88c3d95ea92c8bfc7baca2824
koteshrv <kotesh.rv@gmail.com>
2021-07-13 11:07:23 +0530

    modified config.py

--- config.py
+++ config.py
@@ -7,7 +7,17 @@ MAIL_ID = os.environ.get('MAIL_ID')
 PASSWORD = os.environ.get('PASSWORD')
 TELEGRAM_USER_ID = os.environ.get('TELEGRAM_USER_ID')
 
+# If you want to run locally on your computer
+# Place the chrome driver on path
+# Enter the location of dependencies in default location and add telegram token, mail id
+# password and userid below in quotations
 
 '''
+DEFAULT_LOCATION = '/home/koteshrv/Desktop/herokumeet/dependencies/'
+TIME_TABLE = DEFAULT_LOCATION + 'timetable.xlsx'
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', '')
+MAIL_ID = os.environ.get('MAIL_ID', '')
+PASSWORD = os.environ.get('PASSWORD', '')
+TELEGRAM_USER_ID = os.environ.get('TELEGRAM_USER_ID', '')
 '''
 
